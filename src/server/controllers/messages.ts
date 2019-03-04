27a663f87685f26e85d6bca239ea5cbdddc91b8d
Nikolaus Dunn <nikdunn1979@gmail.com>
2019-03-04 15:01:53 -0500

    added more apis

--- src/server/controllers/messages.ts
+++ src/server/controllers/messages.ts
@@ -3,21 +3,45 @@
 import util = require('util')
 import swaggerTools = require('swagger-tools')
 import db = require('../db')
+import Message = db.Message
 import api = require('../api')
+import turf = require('@turf/turf')
 
 const OK = 200
 const BadRequest = 400
 const InternalServerError = 500
 
-const inspect = (input: any) => util.inspect(input, false, Infinity, true)
+const inspect = (input: any) => util.inspect(input, false, Infinity, false)
 
 // Make sure this matches the Swagger.json body parameter for the /signup API
-interface ListMessagesPayload {
-    searchInfo: swaggerTools.SwaggerRequestParameter<string>
+interface MessagesPayload {
+    lon: swaggerTools.SwaggerRequestParameter<number>,
+    lat: swaggerTools.SwaggerRequestParameter<number>,
+    [paramName: string]: swaggerTools.SwaggerRequestParameter<number> | undefined;
+}
+
+// Make sure this matches the Swagger.json body parameter for the /message GET API
+interface GetMessagePayload {
+    id: swaggerTools.SwaggerRequestParameter<string>,
     [paramName: string]: swaggerTools.SwaggerRequestParameter<string> | undefined;
 }
 
-module.exports.listMessages = function (req: api.Request & swaggerTools.Swagger20Request<ListMessagesPayload>, res: any, next: any) {
+// Make sure this matches the Swagger.json body parameter for the /message GET API
+interface PatchMessagePayload {
+    id: swaggerTools.SwaggerRequestParameter<string>,
+    message: swaggerTools.SwaggerRequestParameter<Message>,
+    [paramName: string]: swaggerTools.SwaggerRequestParameter<string>
+        | swaggerTools.SwaggerRequestParameter<Message>
+        | undefined;
+}
+
+// Make sure this matches the Swagger.json body parameter for the /message DELETE API
+interface DeleteMessagePayload {
+    id: swaggerTools.SwaggerRequestParameter<string>,
+    [paramName: string]: swaggerTools.SwaggerRequestParameter<string> | undefined;
+}
+
+module.exports.messages = function (req: api.Request & swaggerTools.Swagger20Request<MessagesPayload>, res: any, next: any) {
 
     console.log(util.inspect(req.swagger.params, false, Infinity, true))
 
@@ -28,47 +52,92 @@ module.exports.listMessages = function (req: api.Request & swaggerTools.Swagger2
     }
 
     //capture search in variable
-    const incomingSearch = req.swagger.params.searchInfo.value;
+    const lon = req.swagger.params.lon.value;
+    const lat = req.swagger.params.lat.value;
     //if there is a search, match with database docs that belong to that user and put them in array.
     //returned items must belong to the user AND match what was searched in any field belonging to that doc
-    if (incomingSearch) {
+    db.messages.find({}).toArray().then((data) => {
+        if (data) {
+            res.status(OK)
+            res.send(JSON.stringify(data))
+            res.end()
+        }
+        else {
+            res.status(OK)
+            res.send(JSON.stringify([], null, 2))
+            res.end()
+        }
+    }).catch((err) => {
+        res.status(InternalServerError)
+        res.send(JSON.stringify({ message: inspect(err) }, null, 2))
+        res.end()
+    })
+}
 
-        db.messages.find({
-            "feature.properties.text": { '$regex': `.*${incomingSearch}.*`, '$options': 'i' }
-        }).toArray().then((data) => {
-            if (data) {
-                res.status(OK)
-                res.send(JSON.stringify(data))
-                res.end()
-            }
-            else {
-                res.status(OK)
-                res.send(JSON.stringify([], null, 2))
-                res.end()
-            }
-        }).catch((err) => {
-            res.status(InternalServerError)
-            res.send(JSON.stringify({ message: inspect(err) }, null, 2))
-            res.end()
-        })
+module.exports.getMessage = function (req: api.Request & swaggerTools.Swagger20Request<GetMessagePayload>, res: any, next: any) {
+
+    console.log(util.inspect(req.swagger.params, false, Infinity, true))
+
+    res.setHeader('Content-Type', 'application/json')
+
+    if (!req.session) {
+        return
     }
-    //otherwise return all documents belonging to that user
-    else {
-        db.messages.find({}).toArray().then((data) => {
-            if (data) {
-                res.status(OK)
-                res.send(JSON.stringify(data))
-                res.end()
-            }
-            else {
-                res.status(OK)
-                res.send(JSON.stringify([], null, 2))
-                res.end()
-            }
-        }).catch((err) => {
-            res.status(InternalServerError)
-            res.send(JSON.stringify({ message: inspect(err) }, null, 2))
+
+    //capture search in variable
+    const id = req.swagger.params.id.value;
+    //if there is a search, match with database docs that belong to that user and put them in array.
+    //returned items must belong to the user AND match what was searched in any field belonging to that doc
+    db.messages.find({}).toArray().then((data) => {
+        if (data) {
+            res.status(OK)
+            res.send(JSON.stringify(data))
             res.end()
-        })
+        }
+        else {
+            res.status(OK)
+            res.send(JSON.stringify([], null, 2))
+            res.end()
+        }
+    }).catch((err) => {
+        res.status(InternalServerError)
+        res.send(JSON.stringify({ message: inspect(err) }, null, 2))
+        res.end()
+    })
+}
+
+module.exports.patchMessage = function (req: api.Request & swaggerTools.Swagger20Request<PatchMessagePayload>, res: any, next: any) {
+
+    console.log(util.inspect(req.swagger.params, false, Infinity, true))
+
+    res.setHeader('Content-Type', 'application/json')
+
+    if (!req.session) {
+        return
     }
+
+    //capture search in variable
+    const id = req.swagger.params.id.value;
+
+    res.status(InternalServerError)
+    res.send(JSON.stringify({ message: inspect(new Error("Not Implemented")) }, null, 2))
+    res.end()
+}
+
+module.exports.deleteMessage = function (req: api.Request & swaggerTools.Swagger20Request<DeleteMessagePayload>, res: any, next: any) {
+
+    console.log(util.inspect(req.swagger.params, false, Infinity, true))
+
+    res.setHeader('Content-Type', 'application/json')
+
+    if (!req.session) {
+        return
+    }
+
+    //capture search in variable
+    const id = req.swagger.params.id.value;
+
+    res.status(InternalServerError)
+    res.send(JSON.stringify({ message: inspect(new Error("Not Implemented")) }, null, 2))
+    res.end()
 }
