3db23a73daa7c0c8e326bdcd0caeb89aadeaa852
Jasper Chong <jaspercchong@gmail.com>
2019-04-11 11:36:37 -0400

    alleviated contentions between postFavorite and deleteFavorite

--- src/server/controllers/favorites.ts
+++ src/server/controllers/favorites.ts
@@ -20,6 +20,11 @@ interface PostFavoritePayload {
     [paramName: string]: swaggerTools.SwaggerRequestParameter<Favorite> | undefined;
 }
 
+interface DeleteMessagePayload {
+    id: swaggerTools.SwaggerRequestParameter<string>,
+    [paramName: string]: swaggerTools.SwaggerRequestParameter<string> | undefined;
+}
+
 module.exports.getFavorites = function (req: api.Request & swaggerTools.Swagger20Request<FavoritesPayload>, res: any, next: any) {
 
     console.log(util.inspect(req.swagger.params, false, Infinity, true))
@@ -91,5 +96,41 @@ module.exports.postFavorite = function (req: api.Request & swaggerTools.Swagger2
         res.status(api.InternalServerError)
         res.send(JSON.stringify({ message: inspect(err) }, null, 2))
         res.end()
+    })
+}
+
+module.exports.deleteFavorite = function (req: api.Request & swaggerTools.Swagger20Request<DeleteMessagePayload>, res: any, next: any) {
+
+    console.log(util.inspect(req.swagger.params, false, Infinity, true))
+
+    res.setHeader('Content-Type', 'application/json')
+
+    if (!req.session) {
+        res.status(api.InternalServerError)
+        res.send(JSON.stringify({ message: inspect(new Error("No session object exists.")) }, null, 2))
+        return res.end()
+    }
+
+    // capture search in variable
+	const id = req.swagger.params.id.value;
+
+	db.favorites.findOne({ "_id": new mongodb.ObjectID(id)}).then((fav) => {
+
+		// see if we didn't find a message
+		if (!fav) {
+			res.status(api.InternalServerError)
+			res.send(JSON.stringify({ message: inspect(new Error("Favorite not found")) }, null, 2))
+			return res.end()
+        }
+        
+        db.favorites.deleteOne( fav );
+        res.status(api.OK)
+        res.send(JSON.stringify([], null, 2))
+        return res.end()
+		
+	}).catch((err) => {
+        res.status(api.InternalServerError)
+        res.send(JSON.stringify({ message: inspect(err) }, null, 2))
+        return res.end()
     })
 }
