04d265759951ba62a2407888462ec5a01cff1f81
Jasper Chong <jaspercchong@gmail.com>
2019-04-11 11:04:47 -0400

    made deleteFavorite, untested

--- src/server/controllers/favorites.ts
+++ src/server/controllers/favorites.ts
@@ -3,7 +3,6 @@
 import util = require('util')
 import swaggerTools = require('swagger-tools')
 import db = require('../db')
-import Favorite = db.Favorite
 import api = require('../api')
 import mongodb = require('mongodb')
 
@@ -15,9 +14,9 @@ interface FavoritesPayload {
     [paramName: string]: swaggerTools.SwaggerRequestParameter<string> | undefined;
 }
 
-interface PostFavoritePayload {
-    favorite: swaggerTools.SwaggerRequestParameter<Favorite>,
-    [paramName: string]: swaggerTools.SwaggerRequestParameter<Favorite> | undefined;
+interface DeleteMessagePayload {
+    id: swaggerTools.SwaggerRequestParameter<string>,
+    [paramName: string]: swaggerTools.SwaggerRequestParameter<string> | undefined;
 }
 
 module.exports.getFavorites = function (req: api.Request & swaggerTools.Swagger20Request<FavoritesPayload>, res: any, next: any) {
@@ -53,7 +52,7 @@ module.exports.getFavorites = function (req: api.Request & swaggerTools.Swagger2
     })
 }
 
-module.exports.postFavorite = function (req: api.Request & swaggerTools.Swagger20Request<PostFavoritePayload>, res: any, next: any) {
+module.exports.deleteFavorite = function (req: api.Request & swaggerTools.Swagger20Request<DeleteMessagePayload>, res: any, next: any) {
 
     console.log(util.inspect(req.swagger.params, false, Infinity, true))
 
@@ -65,31 +64,26 @@ module.exports.postFavorite = function (req: api.Request & swaggerTools.Swagger2
         return res.end()
     }
 
-    const newFav = {
-        messageId : req.swagger.params.favorite.value.messageId,
-        userId : req.swagger.params.favorite.value.userId
-    }
+    // capture search in variable
+	const id = req.swagger.params.id.value;
 
-    db.favorites.insertOne(newFav).then((result) => {
-        if (result) {
-            db.favorites.findOne({'_id': new mongodb.ObjectID(result.insertedId)}).then((newObject) => {
-                res.status(api.OK)
-                res.send(JSON.stringify(newObject))
-                return res.end()
-            }).catch((err) => {
-                res.status(api.InternalServerError)
-                res.send(JSON.stringify({ message: inspect(err) }, null, 2))
-                return res.end()
-            })
+	db.favorites.findOne({ "_id": new mongodb.ObjectID(id)}).then((fav) => {
+
+		// see if we didn't find a message
+		if (!fav) {
+			res.status(api.InternalServerError)
+			res.send(JSON.stringify({ message: inspect(new Error("Favorite not found")) }, null, 2))
+			return res.end()
         }
-        else {
-            res.status(api.InternalServerError)
-            res.send(JSON.stringify(result))
-            return res.end()
-        }
-    }).catch((err) => {
+        
+        db.favorites.deleteOne( fav );
+        res.status(api.OK)
+        res.send(JSON.stringify([], null, 2))
+        return res.end()
+		
+	}).catch((err) => {
         res.status(api.InternalServerError)
         res.send(JSON.stringify({ message: inspect(err) }, null, 2))
-        res.end()
+        return res.end()
     })
 }
